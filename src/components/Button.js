108738984ad6e2b1db3aec65e8daac8439fc4193
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2023-03-25 03:23:12 +0300

    Update
    Created context.

---
+++ src/components/Button.js
@@ -0,0 +1,14 @@
+import {useContext} from "react";
+import ThemeContext from '../context/ThemeContext';
+
+function Button() {
+    const data = useContext(ThemeContext);
+    console.log(data);
+    return (
+        <div>
+            <p>Button ({data})</p>
+        </div>
+    );
+}
+
+export default Button;
