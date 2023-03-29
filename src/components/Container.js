011f2bce256cd85c2b38751ec23ad5ea06bcb46b
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2023-03-29 20:51:11 +0300

    Update
    Updated image folder.

--- src/components/Container.js
+++ src/components/Container.js
@@ -7,7 +7,7 @@ import ThemeContext from '../context/ThemeContext';
 function Container() {
     const {theme} = useContext(ThemeContext);
     return (
-        <div className={`theme__mode ${theme === 'darkTheme' ? theme : 'lightTheme'}`}>
+        <div className={`theme__clipboard ${theme === 'darkTheme' ? theme : 'lightTheme'}`}>
           <div className="container-fluid">
             <div className="row">
               <div className="col col-12">
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,11 @@ function Header() {
     const {theme, setTheme} = useContext(ThemeContext);
     return (
         <div id="header">
+            <div className="header__content">
+                <div className="img">
+                    <img src="img/logo.svg" alt="logo" />
+                </div>
+            </div>
             <p>Header Active Theme: {theme}</p>
             <button onClick={() => setTheme(theme === 'lightTheme' ? 'darkTheme' : 'lightTheme')}>Change Theme</button>
         </div>
