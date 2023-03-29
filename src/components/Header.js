0d1657bd6a6ec188312ee3585710558ba4ba0e4a
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2023-03-29 21:10:27 +0300

    Update
    Updated header.js for content.

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,13 +5,27 @@ function Header() {
     const {theme, setTheme} = useContext(ThemeContext);
     return (
         <div id="header">
-            <div className="header__content">
-                <div className="img">
-                    <img src="img/logo.svg" alt="logo" />
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 header__content">
+                        <div className="img">
+                            <img src="img/logo.svg" alt="logo" />
+                        </div>
+                        <div className="headline">
+                            <span>A history of everything you copy</span>
+                            <p>Clipboard allows you to track and organize everything you copy. Instantly access your clipboard on all your devices.</p>
+                        </div>
+                        <div className="download">
+                            <button className="btn btn-success">Download for IOS</button>
+                            <button className="btn btn-primary">Download for Mac</button>
+                        </div>
+                    </div>
+                    <div className="col col-12 text-center">
+                        <p>Header Active Theme: {theme}</p>
+                        <button onClick={() => setTheme(theme === 'lightTheme' ? 'darkTheme' : 'lightTheme')}>Change Theme</button>
+                    </div>
                 </div>
             </div>
-            <p>Header Active Theme: {theme}</p>
-            <button onClick={() => setTheme(theme === 'lightTheme' ? 'darkTheme' : 'lightTheme')}>Change Theme</button>
         </div>
     );
 }
