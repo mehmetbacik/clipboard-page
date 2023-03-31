82efa9071f2c73c8fe527dc5a07606d941f71668
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2023-03-31 20:49:00 +0300

    Update
    Updated Main.js for content.

--- src/components/Main.js
+++ src/components/Main.js
@@ -17,7 +17,7 @@ function Main() {
                         </div>
                         <div className="content">
                             <div className="img">
-                                <img src="img/image-computer.png" alt="logo" />
+                                <img src="img/image-computer.png" alt="img" />
                             </div>
                             <div className="text">
                                 <div>
@@ -34,6 +34,48 @@ function Main() {
                                 </div>
                             </div>
                         </div>
+                    </div>
+                    <div className="col col-12 access__clipboard">
+                        <div className="headline">
+                            <span>Access Clipboard Anywhere</span>
+                            <p>Whether you're on the go, or at your computer, you can access all your Clipboard<br className="large" /> 
+                               snippets in a few simple clicks.
+                            </p>
+                        </div>
+                        <div className="content">
+                            <div className="img">
+                                <img src="img/image-devices.png" alt="img" />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col col-12 supercharge__workflow">
+                        <div className="headline">
+                            <span>Supercharge your workflow</span>
+                            <p>We've got the tools to boost your productivity.</p>
+                        </div>
+                        <div className="content">
+                            <div className="detail">
+                                <div className="detail-child">
+                                    <img src="img/icon-blacklist.svg" alt="img" />
+                                    <span>Create blacklists</span>
+                                    <p>Ensure sensitive information never makes its way to your clipboard by excluding certain sources.</p>
+                                </div>
+                                <div className="detail-child">
+                                    <img src="img/icon-text.svg" alt="img" />
+                                    <span>Plain text snippets</span>
+                                    <p>Remove unwanted formatting from copied text for a consistent look.</p>
+                                </div>
+                                <div className="detail-child">
+                                    <img src="img/icon-preview.svg" alt="img" />
+                                    <span>Sneak preview</span>
+                                    <p>Quick preview of all snippets on your Clipboard for easy access.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col col-12 theme__select">
+                        <p className="d-none">Active Theme: {theme}</p>
+                        <button className="btn btn-danger" onClick={() => setTheme(theme === 'lightTheme' ? 'darkTheme' : 'lightTheme')}><i className="theme-icon"/></button>
                     </div>
                 </div>
             </div>
